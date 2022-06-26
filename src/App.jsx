3d1046e54c6efea5fc42637eec4a6da65bd45f44
FreedSoul <mattwork07@gmail.com>
2022-06-26 00:59:50 -0500

    vite+react+tailw working

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Counter from "./components/Counter"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className="bg-slate-500 bg-auto">
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+
+        <div className="flex justify-center w-600 bg-gray-400">
+          <h1 className="font-bold text-2xl text-green-900 ">
+            React and Tailwind with Vitejs!
+          </h1>
+        </div>
+        <Counter />
+      </div>
+    </div>
+  )
+}
+
+export default App
+
