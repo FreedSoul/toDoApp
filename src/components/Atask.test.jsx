ffcf1583f69f5d5075aa9c4fe621430eba5d3390
FreedSoul <mattwork07@gmail.com>
2022-07-15 12:47:44 -0500

    added new test

--- src/components/Atask.test.jsx
+++ src/components/Atask.test.jsx
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest"
 import { render, screen, fireEvent } from "@testing-library/react"
+// import "@testing-library/jest-dom"
 import Atask from "./Atask"
 
 describe("un test", () => {
@@ -8,5 +9,16 @@ describe("un test", () => {
     expect(screen.getByRole("button", { name: "add" })).toBeInTheDocument()
     expect(screen.getByRole("textbox", { name: "" })).toBeInTheDocument()
   })
-  
+
+  it("no deberia insertar tarea nueva si el input esta vacio", () => {
+    render(<Atask />)
+    const addButton = screen.getByRole("button", { name: "add" })
+    const newtaskInput = screen.getByRole("textbox", { name: "" })
+    expect(addButton).toBeInTheDocument()
+    expect(newtaskInput).toBeInTheDocument()
+    fireEvent.change(newtaskInput, { target: { value: "" } })
+    fireEvent.click(addButton)
+
+    expect(screen.queryByRole("heading", { name: "" })).not.toBeInTheDocument()
+  })
 })
